1c57d6b7916941f51a9b1e4775f921783630cbb3
Kris McNair <kmcnair_100@outlook.com>
2024-04-15 11:48:54 -0400

    create functions to get salaries

--- files/modules/WorkAroundModule.js
+++ files/modules/WorkAroundModule.js
@@ -1,33 +1,38 @@
-const getAverageSalaryByRole = role => {
-    const roleData = [];
-    const salariesOfRole = roleData.map(obj => obj.salary);
-    return calculateAverage(salariesOfRole);
-  }
-  
-  const getAverageSalaryByCompany = company => {
-    const companyData = [];
-    const salariesAtCompany = companyData.map(obj => obj.salary);
-    return calculateAverage(salariesAtCompany);
-  }
-  
-  const getSalaryAtCompany = (role, company) => {
-    const companyData = [];
-    const roleAtCompany = companyData.find(obj => obj.role === role);
-    return roleAtCompany.salary;
-  }
-  
+import { getDataByRole, getDataByCompany } from "./salaryData.js";
+import salaryData from "./salaryData.js";
 
-  const getIndustryAverageSalary = () => {
-    const allSalaries = [].map(obj => obj.salary);
-    return calculateAverage(allSalaries);
-  }
-  
-  
-  function calculateAverage(arrayOfNumbers) {
-    let total = 0;
-    arrayOfNumbers.forEach(number => total += number);
-    return (total / arrayOfNumbers.length).toFixed(2);
-  }
-  
-  
-  
+const getAverageSalaryByRole = (role) => {
+  const roleData = getDataByRole(role);
+  const salariesOfRole = roleData.map((obj) => obj.salary);
+  return calculateAverage(salariesOfRole);
+};
+
+const getAverageSalaryByCompany = (company) => {
+  const companyData = getDataByCompany(company);
+  const salariesAtCompany = companyData.map((obj) => obj.salary);
+  return calculateAverage(salariesAtCompany);
+};
+
+const getSalaryAtCompany = (role, company) => {
+  const companyData = getDataByCompany(company);
+  const roleAtCompany = companyData.find((obj) => obj.role === role);
+  return roleAtCompany.salary;
+};
+
+const getIndustryAverageSalary = () => {
+  const allSalaries = salaryData.map((obj) => obj.salary);
+  return calculateAverage(allSalaries);
+};
+
+function calculateAverage(arrayOfNumbers) {
+  let total = 0;
+  arrayOfNumbers.forEach((number) => (total += number));
+  return (total / arrayOfNumbers.length).toFixed(2);
+}
+
+export {
+  getAverageSalaryByRole,
+  getSalaryAtCompany,
+  getIndustryAverageSalary,
+  getAverageSalaryByCompany,
+};
